aa1df895dd1540393985655bee8e409623a5349b
Zicklag <zicklag@katharostech.com>
2018-02-13 17:39:23 -0600

    Remove unused `devServer` config
    Removed `devServer` config as the template is not using the
    Webpack dev server to host the project.

--- template/build/webpack.dev.conf.js
+++ template/build/webpack.dev.conf.js
@@ -15,10 +15,6 @@ Object.keys(baseWebpackConfig.entry).forEach(function (name) {
 module.exports = merge(baseWebpackConfig, {
   // eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
   module: {
     rules: cssUtils.styleRules({
       sourceMap: config.dev.cssSourceMap,
